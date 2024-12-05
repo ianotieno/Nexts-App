3d2562354a9bb108cf19447a2cc3aebe42f1bc0e
Agutu Otieno <agutuotieno@Agutus-MacBook-Air.local>
2024-12-05 15:28:12 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     
     <main>
       <h1>Hello World</h1>
+     
     </main>
   )
 }
